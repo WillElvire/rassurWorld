ca812f7e597765416b8566cd2e5992ff4dc2a8b1
macbook <wkoua@oxylead.net>
2023-04-29 12:36:21 +0000

    feat : assurance

--- src/app/modules/assur.module.ts
+++ src/app/modules/assur.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { AssurAutoComponent } from '../pages/assur-auto/assur-auto.component';
 import { AssurSanteComponent } from '../pages/assur-sante/assur-sante.component';
 import { AssurVoyageComponent } from '../pages/assur-voyage/assur-voyage.component';
+import { AssurRoutingModule } from "../routes/assur.services.routing";
 
 @NgModule({
-  imports : [],
+  imports : [AssurRoutingModule],
   exports : [],
   declarations : [
     AssurAutoComponent,
