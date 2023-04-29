2c980c6959f0c7c91fbe1c2e653b011865173f59
macbook <wkoua@oxylead.net>
2023-04-29 14:23:32 +0000

    feat :routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'assur',
+    loadChildren: () =>
+      import('./modules/assur.module').then((m) => m.AssurModule),
+  },
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
