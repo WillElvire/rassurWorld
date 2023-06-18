78e24f87274a367be7abd41bc5b13fd4476bf7bd
macbook <wkoua@oxylead.net>
2023-06-18 17:51:54 +0000

    feat : stores

---
+++ src/app/store/user$/user.query.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { UserState } from './user.state';
+import { Query, applyTransaction } from '@datorama/akita';
+import { UserStore} from './user.store';
+import { UserDto } from 'src/app/core/interfaces/dto';
+
+
+
+@Injectable()
+export class UserQuery extends Query<UserState> {
+
+  //user$   = this.select();
+  isLoggedIn$ = this.select(state => !!state["user"]);
+  selectUser$ = this.select('user');
+  fullUser$   = this.select(['user', 'token']);
+  isLoading$  = this.selectLoading();
+  selectId$   = this.select(['id']);
+  error$      = this.selectError();
+
+
+  constructor(protected override store: UserStore) {
+    super(store);
+  }
+
+  get isLoggedIn() {
+    const user = this.getValue()["user"];
+    return !!user;
+  }
+
+  get user() {
+    return this.getValue()["user"];
+  }
+
+  update(user : UserDto) {
+    return applyTransaction(()=>{
+      this.store.update(user);
+      this.store.setLoading(true);
+    })
+  }
+
+  logout() {
+    this.store.reset();
+  }
+}
