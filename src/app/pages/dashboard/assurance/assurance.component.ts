d885407c7d290238ab11f0c9aaaa3aa0f241f669
macbook <wkoua@oxylead.net>
2023-08-30 22:44:42 +0000

    feat : frontend updates

--- src/app/pages/dashboard/assurance/assurance.component.ts
+++ src/app/pages/dashboard/assurance/assurance.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { AfterContentInit, Component, inject, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppFacade } from 'src/app/core/facades/app.facade';
 import { UtilsFacades } from 'src/app/core/facades/utils.facade';
 
@@ -8,22 +8,57 @@ import { UtilsFacades } from 'src/app/core/facades/utils.facade';
   templateUrl: './assurance.component.html',
   styleUrls: ['./assurance.component.scss']
 })
-export class AssuranceComponent {
+export class AssuranceComponent  implements OnInit{
 
   private readonly appFacades  = inject(AppFacade);
   private readonly router      = inject(Router);
   private readonly utilsFacade = inject(UtilsFacades);
+  private readonly activatedRo = inject(ActivatedRoute);
   insuranceRequest : any[] = [];
+  fechArgument : string = "unpaid";
+  searchArgument = {
+    active  : "",
+    acepted : "",
+    payed   : ""
+  }
   p: number = 1;
   constructor(){
-    this.loadInsuranceRequest()
+
   }
+
+  ngOnInit(): void {
+      this.activatedRo.params.subscribe((response : any)=> {
+
+        console.log(response.type);
+        this.fechArgument = response.type;
+
+        if(this.fechArgument.includes("active") || this.fechArgument.includes("inactive")){
+          this.searchArgument.active  = this.fechArgument;
+          this.searchArgument.payed   = "unpaid";
+          this.searchArgument.acepted = "unacepted";
+        }
+        if(this.fechArgument.includes("payed") || this.fechArgument.includes("unpaid")){
+          this.searchArgument.payed   = this.fechArgument;
+          this.searchArgument.acepted = "acepted";
+          this.searchArgument.active  = "active";
+        }
+        if(this.fechArgument.includes("acepted") || this.fechArgument.includes("unacepted")){
+          this.searchArgument.acepted = this.fechArgument;
+          this.searchArgument.payed   = "unpaid";
+          this.searchArgument.active  = "active";
+        }
+        this.loadInsuranceRequest();
+      })
+  }
+
+
+
 
   loadInsuranceRequest() {
     this.appFacades.getInsuranceRequest({
-      //active : "active",
-      //payed : "unpaid",
-      //limit : 2
+      active : this.searchArgument.active,
+      payed : this.searchArgument.payed,
+      acepted : this.searchArgument.acepted
     }).subscribe((response : any)=>{
       console.log(response);
       this.insuranceRequest = response.body.returnObject;
