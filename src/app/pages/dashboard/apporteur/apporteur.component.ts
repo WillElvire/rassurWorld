4dcb2f34c120fc55ce5431794c617b1adab1d8fb
macbook <wkoua@oxylead.net>
2023-11-09 22:18:40 +0000

    feat : adding user management to busines account

--- src/app/pages/dashboard/apporteur/apporteur.component.ts
+++ src/app/pages/dashboard/apporteur/apporteur.component.ts
@@ -13,8 +13,10 @@ export class ApporteurComponent implements OnInit {
   private readonly appFacades = inject(AppFacade);
   private readonly utilsFacades = inject(UtilsFacades);
   users : UserDto[] = [];
+  userToEdit !: UserDto ;
   p : number = 1;
   isVisible : boolean = false;
+  isEditVisible : boolean = false;
   code ?: string;
   constructor(){
 
@@ -24,6 +26,18 @@ export class ApporteurComponent implements OnInit {
       this.loadBusinessAccount();
   }
 
+  updateUser(event : any){
+    this.isEditVisible = false;
+    this.appFacades.updateUser(event as any).subscribe(
+       (response)=> {
+        const resp : any = response.body;
+        console.log(resp)
+        this.utilsFacades.successToastMessage("Les informations utilisateur ont été modifiées");
+        this.loadBusinessAccount()
+      },
+       (err)=>  this.utilsFacades.errorToastMessage(!!err.error.message ?  err.error.message :  err.message)
+    )
+  }
   activeUserAccount(id : any,status : any) {
     this.appFacades.activeUserAccount(id,status).subscribe({
       next : (response : any)=> {
@@ -52,11 +66,18 @@ export class ApporteurComponent implements OnInit {
 
   handleOk(): void {
     this.isVisible = false;
+    this.isEditVisible = false;
 
   }
 
   handleCancel(): void {
     this.isVisible = false;
+    this.isEditVisible = false;
+  }
+
+  editUserInformation(user : any) {
+   this.userToEdit = user;
+   this.isEditVisible = true;
   }
 
   viewBusinessDetail(businessCode : any) {
